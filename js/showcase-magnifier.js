470da1c2f6c75918f48ac9c1267e1aed604429b1
Adam Abu Ghaida <adamabughaida@gmail.com>
2022-12-22 21:50:58 +0400

    Finish draft 1
    Finished the first draft of website, grammar+spelling+more to be fixed later.

--- js/showcase-magnifier.js
+++ js/showcase-magnifier.js
@@ -1,31 +1,32 @@
-// $(function() {
-//     $(window).scroll(function() {
-//       var scrollTop = $(document).scrollTop() + ($(window).height() / 3);
-//       var positions = [];
+$(function() {
+    $(window).scroll(function() {
+      var scrollTop = $(document).scrollTop() + ($(window).height() / 3);
+      var positions = [];
   
-//       $('.showcase-slide').each(function() {
-//         //console.log($(this).position().top);
-//         $(this).removeClass("highlight-showcase");
-//         positions.push({
-//           position: $(this).position().top,
-//           element: $(this)
-//         });
-//       });
+      $('.showcase-slide').each(function() {
+        //console.log($(this).position().top);
+        $(this).removeClass("highlight-showcase");
+        positions.push({
+          position: $(this).position().top,
+          element: $(this)
+        });
+      });
   
-//       var getClosest = closest(positions, scrollTop);
-//       console.log(getClosest);
-//       getClosest.addClass("highlight-showcase");
-//     });
+      var getClosest = closest(positions, scrollTop);
+      console.log(getClosest);
+      getClosest.addClass("highlight-showcase");
+    });
   
-//     function closest(array, number) {
-//       var num = 0;
-//       for (var i = array.length - 1; i >= 0; i--) {
-//         if (Math.abs(number - array[i].position) < Math.abs(number - array[num].position)) {
-//           num = i;
-//         }
-//       }
-//       return array[num].element;
-//     }
+    function closest(array, number) {
+      var num = 0;
+      for (var i = array.length - 1; i >= 0; i--) {
+        if (Math.abs(number - array[i].position) < Math.abs(number - array[num].position)) {
+          num = i;
+        }
+      }
+      return array[num].element;
+    }
   
-//   });
+  });
+  
   
